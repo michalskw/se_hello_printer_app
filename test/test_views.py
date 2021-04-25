fdc0639da566dbfe4029707a0da9acc1d7d4ec44
michalskw <michalskw@users.noreply.github.com>
2021-04-25 11:32:35 +0200

    zmiana imienia w xml

--- test/test_views.py
+++ test/test_views.py
@@ -15,4 +15,8 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
-        self.assertEqual(b'{ "imie":"Michal", "mgs":Hello World!"}', rv.data)
+        self.assertEqual(b'{ "imie":"Natalia", "mgs":Hello World!"}', rv.data)
+
+    def test_msg_with_output(self):
+        rv = self.app.get('/?output=xml')
+        self.assertEqual(b'{ "imie":"Natalia", "mgs":Hello World!"}', rv.data)
